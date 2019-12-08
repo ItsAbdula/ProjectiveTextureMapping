f8c34ef4100618cda66d90f3be87c4a6400b7462
백상수 <shirostic@ajou.ac.kr>
2019-12-08 14:04:26 +0900

    add models

--- LearnOpenGL/Source.cpp
+++ LearnOpenGL/Source.cpp
@@ -114,6 +114,35 @@ int main()
 
 			glUseProgram(0);
 		}
+
+		{
+			glUseProgram(lighting);
+
+			auto objectColor = glm::vec3(1.0f, 0.5f, 0.31f);
+			set_uniform_value(lighting, "objectColor", objectColor);
+
+			auto lightColor = glm::vec3(1.0f, 1.0f, 1.0f);
+			set_uniform_value(lighting, "lightColor", lightColor);
+			set_uniform_value(lighting, "lightPos", lightPos);
+			set_uniform_value(lighting, "viewPos", camera.Position);
+
+			glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+			set_uniform_value(lighting, "projection", projection);
+
+			glm::mat4 view = camera.GetViewMatrix();
+			set_uniform_value(lighting, "view", view);
+
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, glm::vec3(0.0f, -15.0f, -40.0f));
+			model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+			model = glm::scale(model, glm::vec3(10, 10, 4));
+			set_uniform_value(lighting, "model", model);
+
+			draw_mesh(*cube);
+
+			glUseProgram(0);
+		}
+
 		{
 			glUseProgram(lighting);
 
@@ -134,8 +163,36 @@ int main()
 			glm::mat4 model = glm::mat4(1.0f);
 			model = glm::translate(model, glm::vec3(0.0f, -10.0f, -40.0f));
 			model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-			model = glm::scale(model, glm::vec3(10, 10, 1));
+			model = glm::scale(model, glm::vec3(12, 12, 1));
 			set_uniform_value(lighting, "model", model);
+
+			draw_mesh(*cube);
+
+			glUseProgram(0);
+		}
+
+		{
+			glUseProgram(lighting);
+
+			auto objectColor = glm::vec3(1.0f, 0.5f, 0.31f);
+			set_uniform_value(lighting, "objectColor", objectColor);
+
+			auto lightColor = glm::vec3(1.0f, 1.0f, 1.0f);
+			set_uniform_value(lighting, "lightColor", lightColor);
+			set_uniform_value(texture_shader, "lightPos", lightPos);
+			set_uniform_value(texture_shader, "viewPos", camera.Position);
+
+			glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+			set_uniform_value(texture_shader, "projection", projection);
+
+			glm::mat4 view = camera.GetViewMatrix();
+			set_uniform_value(texture_shader, "view", view);
+
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, glm::vec3(0.0f, -20.0f, -40.0f));
+			model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+			model = glm::scale(model, glm::vec3(50, 50, 0.5f));
+			set_uniform_value(texture_shader, "model", model);
 
 			draw_mesh(*cube);
 
