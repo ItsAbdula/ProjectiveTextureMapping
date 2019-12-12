2ab55b40650726e1d009f6ea00604e1701d4b9f3
Woohyung Choi <201421062@sce212.ajou.ac.kr>
2019-12-13 02:39:32 +0900

    rename load_image() -> make_image()

--- LearnOpenGL/ResourceLoader.cpp
+++ LearnOpenGL/ResourceLoader.cpp
@@ -194,7 +194,7 @@ bool openObj(const std::string fileName, std::vector<glm::vec3> &vertices, std::
 	return true;
 }
 
-Image *load_Image(std::string fileName, int *width, int *height, int *nrChannels)
+Image *make_Image(std::string fileName, int *width, int *height, int *nrChannels)
 {
 	fileName = "../Images/" + fileName;
 	stbi_set_flip_vertically_on_load(true);
--- LearnOpenGL/ResourceLoader.h
+++ LearnOpenGL/ResourceLoader.h
@@ -38,5 +38,5 @@ glm::vec3 string_to_vec3(std::vector<std::string> &input);
 int parse_lines(std::ifstream &ifs, std::vector<glm::vec3> &vertices, std::vector<glm::vec2> &vertexTexCoord, std::vector<glm::vec3> &vertexNormals);
 std::vector<std::string> parse_line(std::string &line);
 bool openObj(const std::string fileName, std::vector<glm::vec3> &vertices, std::vector<glm::vec2> &vertexTexCoord, std::vector<glm::vec3> &vertexNormals);
-Image *load_Image(std::string fileName, int *width, int *height, int *nrChannels);
+Image *make_Image(std::string fileName, int *width, int *height, int *nrChannels);
 void free_image(Image *img);
